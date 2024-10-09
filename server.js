77b638c3791d26aa78d521a180fcf85aa05c97f5
medicentecno <medicentecno@outlook.com>
2024-10-09 12:40:45 -0300

    Endpoint para renderizar el formulario de alta de materias

--- server.js
+++ server.js
@@ -18,11 +18,24 @@ const servidor = http.createServer((req, res) => {
             res.writeHead(200, { 'Content-Type': 'text/html' });
             res.end(data);
         });
-    } else {
-        // Responder con un 404 para otras rutas
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end('Ruta no encontrada');
     }
+
+    if (req.method === 'GET' && req.url === '/formulario/alta') {
+        const filePath = path.join(__dirname, 'alta.html');
+
+        // Leer el archivo HTML
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error al cargar el archivo');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    }
+
+
 });
 
 servidor.listen(PORT, () => {
