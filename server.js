9c55f372d4a337662116ff2fc3749fc56c528af4
medicentecno <medicentecno@outlook.com>
2024-10-09 12:32:06 -0300

    Servidor base en NodeJS

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const PORT = 3000;
+const fs = require('fs');
+const path = require('path');
+
+const servidor = http.createServer((req, res) => {
+    // Manejar la solicitud GET a la ruta "/"
+    if (req.method === 'GET' && req.url === '/') {
+        const filePath = path.join(__dirname, 'index.html');
+
+        // Leer el archivo HTML
+        fs.readFile(filePath, (err, data) => {
+            if (err) {
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('Error al cargar el archivo');
+                return;
+            }
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            res.end(data);
+        });
+    } else {
+        // Responder con un 404 para otras rutas
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Ruta no encontrada');
+    }
+});
+
+servidor.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}.`);
+})
